724fe1b3f534950be2afd18d18e3512dba416e9c
Quang Ha <quanghadang08@gmail.com>
2021-04-20 01:00:28 +0700

    update

--- Calendar.c
+++ Calendar.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
 #include<string.h>
-struct calendar{
+typedef struct calendar{
     char* name_job;
     char* day;
     char* month;
     char* hours;
-};
-void split
-void add_entry
+}calendar;
+void splitString(char* line){}
+void add_entry(calendar calendar){}
+void 
 int main(){
 
 }
